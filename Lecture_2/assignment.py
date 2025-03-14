fa550887f86a33b6bc6e2fc7570ce6503fdef078
Nahyaan Naveed <nahyaannvd@outlook.com>
2025-03-14 21:29:04 +0500

    Lec 2

---
+++ Lecture_2/assignment.py
@@ -0,0 +1,16 @@
+
+# %%
+first_name = input ("Enter your first name: ")
+last_name = input ("Enter your last name: ")
+
+first_name = first_name.upper()
+last_name = last_name.lower()
+
+total_letters = len (first_name) + len (last_name)
+
+print ("Your first name in capitals is:", first_name)
+print ("Your last name in smalls is:", last_name)
+print ("Your name has", total_letters, "letters in it.")
+
+
+# %%
